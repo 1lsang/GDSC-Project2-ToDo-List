fa3a4afab46a8b428358759fc42f32590d66f1b6
1lsang <ilsangpark.98@gmail.com>
2022-11-14 15:27:49 +0900

    :sparkles: fix:  constructor

--- src/core/Component.js
+++ src/core/Component.js
@@ -5,9 +5,17 @@ export default class Component {
 
     props
 
-    constructor(target, props) {
+    component
+
+    constructor(target, props, tag = "div") {
+        this.component = document.createElement(tag)
+
         this.target = target
+        this.target.appendChild(this.component)
+
         this.props = props
+        this.setup()
+        this.setEvent()
         this.render()
     }
 
@@ -16,7 +24,32 @@ export default class Component {
         return ``
     }
 
+    // eslint-disable-next-line class-methods-use-this
+    setup() {}
+
     render() {
-        this.target.innerHTML = this.template()
+        this.component.innerHTML = this.template()
+        this.mounted()
+    }
+
+    // eslint-disable-next-line class-methods-use-this
+    mounted() {}
+
+    // eslint-disable-next-line class-methods-use-this
+    setEvent() {}
+
+    addEvent(eventType, selector, callback) {
+        const children = [...document.querySelectorAll(selector)]
+        const isTarget = (target) =>
+            children.includes(target) || target.closest(selector)
+        this.component.addEventListener(eventType, (event) => {
+            if (!isTarget(event.target)) return false
+            callback(event)
+        })
+    }
+
+    setState(newState) {
+        this.state = { ...this.state, ...newState }
+        this.render()
     }
 }
